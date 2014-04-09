5dea044ba3b9af908f1d6329efd1c53c3c6b1c30
fsolt <frederick-solt@uiowa.edu>
2014-04-09 15:51:29 -0500

    initial commit

---
+++ linear_regression2.R
@@ -0,0 +1,26 @@
+###  Classical Linear Regression, Assumptions, and Violations (Part 2)
+
+# What are the most important assumptions of the linear regression model?
+# And how do we diagnose violations and correct problems?  
+
+# 1. VALIDITY!
+# The single most important issue encountered in doing regression analysis regards
+# validity: the fit between the measures employed and the concepts that one seeks
+# to investigate.  This is frequently skipped over in discussions of
+# methods because problems of validity are often difficult to overcome.
+# Nevertheless, the quality of your data should be your foremost concern (Gelman
+# and Hill 2007, 45-46).
+
+# 2. LINEARITY and ADDITIVITY
+# Recall that the regression model is of the form y = XB + e. It assumes that the
+# DV is appropriately modeled as a linear function of the IVs--that any
+# similarly-sized change in an IV predicts the same amount of change in the DV.
+# One should consider theoretical reasons this may not be so (e.g., one might 
+# expect 30-year-olds to participate in politics more than 20-year-olds, but 
+# 80-year-olds to participate *less* than 70-year-olds).  We can also check
+# the linearity assumption empirically.
+
+
+
+
+
